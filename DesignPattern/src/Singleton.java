51e689d319a01bca040d57bab491a85c9d3407df
charlseo <charseo@gmail.com>
2015-07-16 16:02:45 +1000

    initial commit

---
+++ DesignPattern/src/Singleton.java
@@ -0,0 +1,27 @@
+
+public class Singleton {
+	private static Singleton singleton;
+	private static Object classLock = Singleton.class;
+	
+	private long wipMoves;
+	
+	public static Singleton getSingleton(){
+		synchronized(classLock) {
+			if (singleton == null) {
+				singleton = new Singleton();
+				System.out.println("Singleton created");
+			} else {
+				System.out.println("Singleton already created");
+			}
+		}
+		
+		return singleton;
+	}
+	
+	public void recordWipMove(){
+		synchronized(classLock) {
+			wipMoves ++;
+			System.out.println(wipMoves);
+		}
+	}
+}
---
+++ DesignPattern/src/SingletonMain.java
@@ -0,0 +1,14 @@
+
+public class SingletonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final Singleton singleton = new Singleton();
+		singleton.getSingleton();
+		singleton.recordWipMove();
+		singleton.getSingleton();
+		singleton.recordWipMove();
+	}
+
+}
